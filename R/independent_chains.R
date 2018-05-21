8d4deb5737c94d03c15ee0067723dfed11607352
Soulivanh Thao <kajhiua@hotmail.com>
2018-05-21 20:02:25 +0200

    lposterior_6

--- R/independent_chains.R
+++ R/independent_chains.R
@@ -7,15 +7,15 @@ create_gen_chain <- function(compute_nextmu){
     mu_res[1, ] <- mu
     x_res[1, ,] <- draw_proposals(n = M, mu, sigma)
     pi_res[1, ] <- eval_logposterior(x_res[1, ,], logposterior)
-    denom_res[1, ] <- log(eval_proposals(x_res[1, ,], mu, sigma))
+    #denom_res[1, ] <- log(eval_proposals(x_res[1, ,], mu, sigma))
     for(t in seq.int(T)[-1]){
       weights <- normalize_weights(compute_weights(pi_res[t-1, ], denom_res[t-1, ]))
       mu_res[t, ] <- compute_nextmu(x_res[t-1, ,], weights)
       x_res[t, ,] <- draw_proposals(n = M, mu_res[t, ], sigma)
       pi_res[t, ] <- eval_logposterior(x_res[t, ,], logposterior)
-      denom_res[t, ] <- log(eval_proposals(x_res[t, ,], mu_res[t, ], sigma))
+     # denom_res[t, ] <- log(eval_proposals(x_res[t, ,], mu_res[t, ], sigma))
     }
-    list(mu = mu_res, x = x_res, pi = pi_res, denom = denom_res)
+    list(mu = mu_res, x = x_res, pi = pi_res)#, denom = denom_res)
   }
 }
 gen_chain_apis <- create_gen_chain(compute_nextmu_apis)
@@ -25,13 +25,13 @@ gen_chain_mcmc <- function(T = 100, M = 2, mu, sigma, sigma_mh = sigma, logposte
   mu_res <- simple_MH(logposterior = logposterior, d = length(mu), T = T, mu = mu, sigma = sigma_mh)
   x_res <- array(dim = c(T, length(mu), M))
   pi_res <- array(dim = c(T, M))
-  denom_res <- array(dim = c(T, M))
+  #denom_res <- array(dim = c(T, M))
   for(t in seq.int(T)){
     x_res[t, ,] <- draw_proposals(n = M, mu_res[t, ], sigma)
-    denom_res[t, ] <- eval_proposals(x_res[t, ,], mu_res[t, ], sigma)
+    #denom_res[t, ] <- eval_proposals(x_res[t, ,], mu_res[t, ], sigma)
     pi_res[t, ] <- eval_logposterior(x_res[t, ,], logposterior)
   }
-  list(mu = mu_res, x = x_res, pi = pi_res, denom = denom_res)
+  list(mu = mu_res, x = x_res, pi = pi_res)#, denom = denom_res)
 }
 
 make_parallel_chain <- function(gen_chain){
@@ -40,15 +40,15 @@ make_parallel_chain <- function(gen_chain){
     mu_res <- array(dim = c(T, d, N))
     x_res <- array(dim = c(T, d, M, N))
     pi_res <- array(dim = c(T, M, N))
-    denom_res <- array(dim = c(T, M, N))
+    #denom_res <- array(dim = c(T, M, N))
     for(n in seq.int(N)){
       chain <- gen_chain(T = T, M = M, mu = init_mu[, n], sigma = sigma, logposterior = logposterior, ...)
       mu_res[,, n] <- chain$mu
       x_res[,,, n] <- chain$x
       pi_res[,, n] <- chain$pi
-      denom_res[,, n] <- chain$denom
+      #denom_res[,, n] <- chain$denom
     }
-    list(mu = mu_res, x = x_res, pi = pi_res, denom = denom_res)
+    list(mu = mu_res, x = x_res, pi = pi_res)#, denom = denom_res)
   }
 }
 indep_chains_mcmc <- make_parallel_chain(gen_chain_mcmc)
